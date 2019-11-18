9d2a213df8dab271189281ba93657eda9cca1367
nilesh <nilesh.kant@sakhatech.com>
2019-11-18 17:04:21 +0530

    added script

--- src/api/services/channelList/channelList.route.js
+++ src/api/services/channelList/channelList.route.js
@@ -11,6 +11,27 @@ const router = express.Router();
 
 router
   .route('/:pageno/:pagesize')
+  /**
+   * @api {get} v1/channel-list/:pageno/:pagesize Channel List
+   * @apiDescription Get channel list with pagination
+   * @apiVersion 1.0.0
+   * @apiName Channel list
+   * @apiGroup Channel
+   * @apiPermission all
+   *
+   * @apiParam  {String}  pageno    Page Number
+   * @apiParam  {String}  pagesize  Page Size
+   *
+   * @apiSuccess {String}  Broadcaster Broadcaster
+   * @apiSuccess {String}  Channel     Channel
+   * @apiSuccess {String}  HD          HD
+   * @apiSuccess {String}  category    category
+   * @apiSuccess {String}  imageurl    imageurl
+   * @apiSuccess {String}  language    language
+   * @apiSuccess {String}  price       price
+   *
+   * @apiError (Unauthorized 401)  Unauthorized  Only authenticated Users can access the data
+   */
   .get(controller.list);
 
 module.exports = router;
